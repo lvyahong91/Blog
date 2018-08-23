984a5733c88b2c4458c2b5bbc58870d039805efa
TeacherCangNulling <3218870799@qq.com>
2018-08-23 15:28:39 +0800

    博主信息管理

--- src/main/java/com/xqc/controller/admin/BloggerAdminController.java
+++ src/main/java/com/xqc/controller/admin/BloggerAdminController.java
@@ -1,15 +1,21 @@
 package com.xqc.controller.admin;
 
+import java.io.File;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.xqc.entity.Blogger;
 import com.xqc.service.BloggerService;
 import com.xqc.util.CryptographyUtil;
+import com.xqc.util.DateUtil;
 import com.xqc.util.ResponseUtil;
 
 import net.sf.json.JSONObject;
@@ -57,6 +63,49 @@ public class BloggerAdminController {
 	public String  logout()throws Exception{
 		SecurityUtils.getSubject().logout();
 		return "redirect:/login.jsp";
+	}
+	
+	
+	/**
+	 * 修改博主信息
+	 * @param file1
+	 * @param blogger
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/save")
+	public String save(@RequestParam("imageFile") MultipartFile imageFile,Blogger blogger,HttpServletRequest request,HttpServletResponse response)throws Exception{
+		if(!imageFile.isEmpty()){
+			String filePath=request.getServletContext().getRealPath("/");
+			String imageName=DateUtil.getCurrentDateStr()+"."+imageFile.getOriginalFilename().split("\\.")[1];
+			imageFile.transferTo(new File(filePath+"static/userImages/"+imageName));
+			blogger.setImageName(imageName);
+		}
+		int resultTotal=bloggerService.update(blogger);
+		StringBuffer result=new StringBuffer();
+		if(resultTotal>0){
+			result.append("<script language='javascript'>alert('修改成功！');</script>");
+		}else{
+			result.append("<script language='javascript'>alert('修改失败！');</script>");
+		}
+		ResponseUtil.write(response, result);
+		return null;
+	}
+	
+	/**
+	 * 查询博主信息
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/find")
+	public String find(HttpServletResponse response)throws Exception{
+		Blogger blogger=bloggerService.find();
+		JSONObject jsonObject=JSONObject.fromObject(blogger);
+		ResponseUtil.write(response, jsonObject);
+		return null;
 	}
 
 	
